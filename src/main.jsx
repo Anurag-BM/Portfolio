c76358efdc048360b86344b40fb1d7772bfb997c
Anurag-BM <anuragmahanthesh69@gmail.com>
2026-04-16 12:25:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Portfolio from './Portfolio'
+import ArticlePage from './ArticlePage'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Portfolio />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Portfolio />} />
+      <Route path="/article/:id" element={<ArticlePage />} />
+    </Routes>
+  </BrowserRouter>
+)
